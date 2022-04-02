894328360057bfbda0d4cae3e3b8a3045d0940bd
Matias Coronel <97346804+MatiasCoronel1312@users.noreply.github.com>
2022-04-02 20:05:44 -0300

    operadores avanzados JS

---
+++ js/array.js
@@ -0,0 +1,134 @@
+class Prestamo {
+    constructor(nombre, apellido, prestamoNeto, cuotas) {
+        this.nombre = nombre.toUpperCase();
+        this.apellido = apellido.toUpperCase();
+        this.prestamoNeto = parseFloat(prestamoNeto);
+        this.cuotas = parseInt(cuotas);
+        
+    }
+
+    InteresBanco() {
+        //segun cantidad de cuotas el interes aumenta   
+        if (this.cuotas <= 3) {
+            interes = 1.5;
+        } else if ((this.cuotas > 3) && (this.cuotas <= 6)) {
+            interes = 2;
+        } else if ((this.cuotas > 6) && (this.cuotas <= 9)) {
+            interes = 2.5;
+        } else {
+            interes = 3;
+        }
+        this.totalAPagar = this.prestamoNeto * interes;
+       
+    }
+}
+
+let inputNombre = document.getElementById("nombre")
+
+inputNombre.onkeyup = (event) => {
+    nombre = event.target.value;
+    console.clear();
+    console.log(nombre);
+
+}
+
+let inputApellido = document.getElementById("apellido")
+
+inputApellido.onkeyup = (event) => {
+    apellido = event.target.value;
+    console.clear();
+    console.log(apellido);
+}
+let inputSueldo = document.getElementById("sueldo")
+
+inputSueldo.onkeyup = (event) => {
+    sueldo = event.target.value;
+    console.clear();
+    console.log(parseFloat(sueldo));
+    return sueldo;
+}
+
+let inputCuotas = document.getElementById("select")
+inputCuotas.onchange = (event) => {
+    cuotas = event.target.value;
+    console.log(parseInt(cuotas));
+
+}
+
+//debugger
+boton.addEventListener("click", () => {
+    console.log("Click");
+    saludo.innerHTML = '<h2>Hola ' + nombre + " " + apellido + ' , Bienvenido!</h2>';
+})
+
+const botonSimular = document.getElementById("botonSimular");
+botonSimular.addEventListener("click", () => {
+
+
+    
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Sus datos se estan procesando',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    
+   
+    const prestamo1 = new SimuladorPrestamo(sueldo, sueldoDebitar, cuotas);
+
+    prestamo1.cotizarPrestamo();
+
+    prestamoNeto = prestamo1.cotizarPrestamo();
+    const prestamos = [];
+    // prestamos.push(new Prestamo("matias", "coronel", "300000", 12));
+    // prestamos.push(new Prestamo("martin", "perez", "25000", 6));
+    // prestamos.push(new Prestamo("marcos", "lozada", "120000", 12));
+    // prestamos.push(new Prestamo("alicia", "rivero", "75000", 9));
+    // prestamos.push(new Prestamo("laura", "gomez", "9000", 3));
+    prestamos.push(new Prestamo(nombre, apellido, prestamoNeto, cuotas)); //el ultimo es el que se crea con los datos ingresados
+
+
+    for (const prestamo of prestamos) {
+        prestamo.InteresBanco();
+        
+    }
+
+     for (let cliente = 0; cliente < prestamos.length; cliente++) {
+       console.log(prestamos[cliente]);
+    
+      
+    }
+    console.log(prestamos[0].totalAPagar)
+    // const clientes = prestamos.map((el) => el.nombre);
+    // console.log(clientes); //y una lista por nombre de clientes
+
+    TotalPrestamo.innerHTML = '<h3>Monto total del prestamo permitido: $' + prestamoNeto + '</h3>';
+    const buttonDetalle = document.querySelector("#buttonDetalle")
+    buttonDetalle.innerHTML=  '<button id="botonDetalle"><a href="./assets/pages/detalles.html">Detalle</a></button>';
+     ImporteCuotas= prestamos[0].totalAPagar/cuotas;
+      console.log(ImporteCuotas)
+    // guardoDatos();
+     guardoDatosJSON();
+     
+})
+    
+
+
+// const guardoDatos = () => {
+    
+
+//     localStorage.setItem("Nombre",nombre);
+//     localStorage.setItem("Apellido",apellido);
+//     localStorage.setItem("PrestamoNeto",prestamoNeto);
+//     localStorage.setItem("Cuotas",cuotas);
+//     localStorage.setItem("ImporteCuotas",ImporteCuotas);
+//     console.info("Se almaceno correctamente");
+    
+// }
+
+
+
+
+
+
---
+++ js/json.js
@@ -0,0 +1,41 @@
+const guardoDatosJSON = () => {
+    datosCliente = {
+        nombre: nombre.trim(),
+        apellido: apellido.trim(),
+        prestamoNeto: parseFloat(prestamoNeto),
+        cuotas: parseInt(cuotas),
+        ImporteCuotas: parseFloat(ImporteCuotas)
+    }
+    datos.push(datosCliente)
+    localStorage.setItem("datosDelCliente", JSON.stringify(datos));
+    console.info("Se almaceno correctamente json");
+}
+
+
+const reset = document.getElementById("reset");
+reset.addEventListener("click", () => {
+//agregue la libreria sweet alert para la confirmacion antes de borrar los datos
+    const resp =Swal.fire({
+        title: '¿Realmente desea eliminar los datos almacenados?',
+        
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: 'grey',
+        cancelButtonColor: '#ad2424',
+        confirmButtonText: 'Si, borrar todo!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.clear();
+            formulario.reset();
+          Swal.fire(
+            'Borrado!',
+            'Tus datos han sido borrado.',
+            'success'
+            
+            
+          )
+        }
+      })
+
+})
+
