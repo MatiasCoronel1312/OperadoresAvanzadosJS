91786eac1acf7a3c44074ee52776a448426170ff
Matias Coronel <97346804+MatiasCoronel1312@users.noreply.github.com>
2022-04-09 20:26:16 -0300

    cambie al api de json placeholder

--- js/fetch.js
+++ js/fetch.js
@@ -13,39 +13,42 @@ botonEnviarDatos.addEventListener("click", () => {
 
     }
 
-    // fetch('https://jsonplaceholder.typicode.com/posts', {
-    //         method: 'POST',
-    //         body: JSON.stringify(datosCliente),
-    //         headers: {
-    //             'Content-type': 'application/json; charset=UTF-8',
-    //         },
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(datosCliente),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        
+
+
+
+        //peticionEnviada.innerHTML = "En breve recibira un email del banco"
+
+    // fetch('https://matiascoronel1312.github.io/OperadoresAvanzadosJS/datos.json', {
+    //     mode: 'no-cors',
+    //     method: 'POST',
+    //     body: JSON.stringify(datosCliente),
+    //     //headers: {'Content-type': 'application/json',  },
+    // })
+    //     .then( (response)=> {
+
+    //         if (response.ok) {
+    //             return response.text()
+    //         } else {
+    //             throw "Error en la llamada ajax";
+    //         }
+
     //     })
-    //     .then((response) => response.json())
-    //     .then((data) => console.log(data))
-
-
-
-    fetch('https://matiascoronel1312.github.io/OperadoresAvanzadosJS/datos.json', {
-        mode: 'no-cors',
-        method: 'POST',
-        body: JSON.stringify(datosCliente),
-        //headers: {'Content-type': 'application/json',  },
-    })
-        .then(function (response) {
-
-            if (response.ok) {
-                return response.text()
-            } else {
-                throw "Error en la llamada ajax";
-            }
-
-        })
-        .then((text) => {
-            console.log(text);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    //     .then((text) => {
+    //         console.log(text);
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
 
 
 })
